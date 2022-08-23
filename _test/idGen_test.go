2d1d842ef4a986b34115e5498ee126779105b5d6
Emilis <emilis.mitrikas@gmail.com>
2022-08-23 18:29:31 +0100

    Tests moved into separate directory withing the module. This will allow testing the module as if a client was using it

--- _test/idGen_test.go
+++ _test/idGen_test.go
@@ -1,6 +1,7 @@
-package idGen
+package _test
 
 import (
+	"github.com/emillis/idGen"
 	"testing"
 )
 
@@ -9,64 +10,64 @@ func TestStatic(t *testing.T) {
 	salt := "156vs/d1ce5_35ctRF&^£$fDFS5RV4531dfv1r4w5e6f1d1\\5sdD5we(fE\",fe3s5EF"
 
 	tests := []struct {
-		cfg  Requirements
+		cfg  idGen.Requirements
 		want string
 	}{
 		{
-			Requirements{64, salt, nil, nil, nil},
+			idGen.Requirements{64, salt, nil, nil, nil},
 			"NwiKlYB4xFOeEc5Fy-0I4NGCg0nvTWWRc-aAftaZ9KUTjVN5n1RAcYr23CEFm-Mr",
 		},
 		{
-			Requirements{64, salt, LowerOnly, nil, nil},
+			idGen.Requirements{64, salt, idGen.LowerOnly, nil, nil},
 			"nwiklyb4xfoeec5fy-0i4ngcg0nvtwwrc-aaftaz9kutjvn5n1racyr23cefm-mr",
 		},
 		{
-			Requirements{64, salt, UpperOnly, nil, nil},
+			idGen.Requirements{64, salt, idGen.UpperOnly, nil, nil},
 			"NWIKLYB4XFOEEC5FY-0I4NGCG0NVTWWRC-AAFTAZ9KUTJVN5N1RACYR23CEFM-MR",
 		},
 
 		{
-			Requirements{64, salt, nil, AlphaOnly, nil},
+			idGen.Requirements{64, salt, nil, idGen.AlphaOnly, nil},
 			"NwiKlYBxFOeEcFyINGCgnvTWWRcaAftaZKUTjVNnRAcYrCEFmMrwxOcDBJryYZKT",
 		},
 		{
-			Requirements{64, salt, LowerOnly, AlphaOnly, nil},
+			idGen.Requirements{64, salt, idGen.LowerOnly, idGen.AlphaOnly, nil},
 			"nwiklybxfoeecfyingcgnvtwwrcaaftazkutjvnnracyrcefmmrwxocdbjryyzkt",
 		},
 		{
-			Requirements{64, salt, UpperOnly, AlphaOnly, nil},
+			idGen.Requirements{64, salt, idGen.UpperOnly, idGen.AlphaOnly, nil},
 			"NWIKLYBXFOEECFYINGCGNVTWWRCAAFTAZKUTJVNNRACYRCEFMMRWXOCDBJRYYZKT",
 		},
 
 		{
-			Requirements{64, salt, nil, NumericOnly, nil},
+			idGen.Requirements{64, salt, nil, idGen.NumericOnly, nil},
 			"4504095123896559496345396855855513224654178015435189919615859574",
 		},
 		{
-			Requirements{64, salt, LowerOnly, NumericOnly, nil},
+			idGen.Requirements{64, salt, idGen.LowerOnly, idGen.NumericOnly, nil},
 			"4504095123896559496345396855855513224654178015435189919615859574",
 		},
 		{
-			Requirements{64, salt, UpperOnly, NumericOnly, nil},
+			idGen.Requirements{64, salt, idGen.UpperOnly, idGen.NumericOnly, nil},
 			"4504095123896559496345396855855513224654178015435189919615859574",
 		},
 
 		{
-			Requirements{64, salt, nil, AlphanumericOnly, nil},
+			idGen.Requirements{64, salt, nil, idGen.AlphanumericOnly, nil},
 			"NwiKlYB4xFOeEc5Fy0I4NGCg0nvTWWRcaAftaZ9KUTjVN5n1RAcYr23CEFmMrwxO",
 		},
 		{
-			Requirements{64, salt, LowerOnly, AlphanumericOnly, nil},
+			idGen.Requirements{64, salt, idGen.LowerOnly, idGen.AlphanumericOnly, nil},
 			"nwiklyb4xfoeec5fy0i4ngcg0nvtwwrcaaftaz9kutjvn5n1racyr23cefmmrwxo",
 		},
 		{
-			Requirements{64, salt, UpperOnly, AlphanumericOnly, nil},
+			idGen.Requirements{64, salt, idGen.UpperOnly, idGen.AlphanumericOnly, nil},
 			"NWIKLYB4XFOEEC5FY0I4NGCG0NVTWWRCAAFTAZ9KUTJVN5N1RACYR23CEFMMRWXO",
 		},
 	}
 
 	for _, test := range tests {
-		got := Static(val, &test.cfg)
+		got := idGen.Static(val, &test.cfg)
 
 		if got != test.want {
 			t.Errorf("expected %s, got %s\n", test.want, got)
@@ -79,7 +80,7 @@ func TestRandom(t *testing.T) {
 	output := make(map[string]string)
 
 	for i := 0; i < 20; i++ {
-		got := Random(&Requirements{desiredIDLength, "", nil, nil, nil})
+		got := idGen.Random(&idGen.Requirements{desiredIDLength, "", nil, nil, nil})
 		gotLen := len(got)
 
 		if gotLen != desiredIDLength {
